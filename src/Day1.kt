2584cfc4742e1cac933c056fe3cc762ccb2fd240
michael.kubovic <michael.kubovic@websupport.sk>
2024-12-03 00:00:23 +0100

    day 1 b

--- src/Day1.kt
+++ src/Day1.kt
@@ -2,13 +2,10 @@ import java.io.File
 import kotlin.math.max
 import kotlin.math.min
 
-fun main() {
-    val lines = File("input/day1.txt").readLines()
+fun main() = day1b()
 
-    val numbers = lines.asSequence().map {
-        val ints = it.split("   ").map { it.toInt() }
-        Pair(ints[0], ints[1])
-    }.unzip()
+fun day1() {
+    val numbers = readNumbers()
 
     val lefts = numbers.first.sorted()
     val rights = numbers.second.sorted()
@@ -18,4 +15,26 @@ fun main() {
     }.sum()
 
     println("sum of diffs is $sum")
+}
+
+private fun readNumbers(): Pair<List<Int>, List<Int>> {
+    val lines = File("input/day1.txt").readLines()
+
+    val numbers = lines.asSequence().map {
+        val ints = it.split("   ").map { it.toInt() }
+        Pair(ints[0], ints[1])
+    }.unzip()
+    return numbers
+}
+
+fun day1b() {
+    val numbers = readNumbers()
+
+    val similarityScore = numbers.first.asSequence().map {
+        val needle = it
+        val found = numbers.second.asSequence().filter { it == needle }.count()
+        it * found
+    }.sum()
+
+    println("similarity score $similarityScore")
 }
