2680c83a9e29d6c83991d9d32ec85c111533726e
michael.kubovic <michael.kubovic@websupport.sk>
2024-12-04 22:09:26 +0100

    day 2 part 2

--- src/Day2.kt
+++ src/Day2.kt
@@ -1,26 +1,46 @@
 import java.io.File
 
-fun main() = day2()
+fun main() = day2part2()
+
+private fun reports(): List<List<Int>> {
+    val lines = File("input/day2.txt").readLines()
+    return lines.map { it.split(" ").map(String::toInt) }
+}
 
 fun day2() {
-    val lines = File("input/day2.txt").readLines()
-    val safeReports = lines.asSequence().map(::evaluateReport).filter { it }.count()
+    val safeReports = reports().map(::evaluateReport).count { it }
     println("safe reports $safeReports")
 }
 
-private fun evaluateReport(report: String): Boolean {
-    val levels = report.split(" ").map(String::toInt)
-
+private fun evaluateReport(levels: List<Int>): Boolean {
     if (levels != levels.sorted() && levels != levels.sortedDescending()) {
         return false
     }
 
-    return levels.indices.take(levels.size - 1).map {
+    val safe = levels.indices.take(levels.size - 1).map {
         val level = levels[it]
         val nextLevel = levels[it + 1]
 
         if (level > nextLevel) level - nextLevel else nextLevel - level
-    }.fold(true) {
-        acc, diff -> acc && diff >= 1 && diff <= 3
-    }
+    }.fold(true) { acc, diff -> acc && diff in 1..3 }
+//    println("levels $levels are safe: $safe")
+    return safe
+}
+
+fun day2part2() {
+    val safeReports = reports().map { report ->
+        if (evaluateReport(report)) {
+            return@map true
+        }
+
+        report.indices.map {
+            val dampenedReport = report.toMutableList()
+            dampenedReport.removeAt(it);
+            dampenedReport
+        }
+        .map(::evaluateReport)
+        .firstOrNull { it } != null
+    }.count { it }
+
+    println("safe reports with the Problem Dampener $safeReports")
 }
